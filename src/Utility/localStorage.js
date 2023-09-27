34658bd1f51d56987ca43b65c08c58b7aca21a99
marizal-akib <marizalakib@gmail.com>
2023-09-27 06:22:52 +0100

    make localstorage function

---
+++ src/Utility/localStorage.js
@@ -0,0 +1,20 @@
+const getDonation = () =>{
+    const storedDonation = localStorage.getItem('donate-fund');
+
+    if(storedDonation){
+        return JSON.parse(storedDonation)
+    }
+    return [];
+}
+
+const saveDonation = id =>{
+    const storedDonation = getDonation();
+    const exist = storedDonation.find(donationId => donationId === id)
+
+    if(!exist){
+        storedDonation.push(id);
+        localStorage.setItem('donate-fund',JSON.parse(storedDonation))
+    }
+}
+
+export {getDonation , saveDonation}
