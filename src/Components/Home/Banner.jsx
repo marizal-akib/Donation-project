465ba4480955c5b03a21fa76ac59d7e205be619d
marizal-akib <marizalakib@gmail.com>
2023-09-28 02:34:07 +0100

    setup search logic

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,18 +1,39 @@
-const Banner = () => {
+/* eslint-disable react/prop-types */
+
+const Banner = ({getKeyword}) => {
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getKeyword(e.target.keyword.value);
+  };
+
   return (
     <div
       className="hero min-h-screen -mt-20"
       style={{
-        backgroundImage:
-          "url(https://i.ibb.co/z7qVxwH/banner.webp)",
+        backgroundImage: "url(https://i.ibb.co/z7qVxwH/banner.webp)",
       }}
     >
       <div className="hero-overlay bg-slate-300 bg-opacity-40"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-4xl font-bold w-fit text-black">I Grow By Helping People In Need</h1>
-          <form><input className="p-2 text-black rounded-l-lg  w-56" type="text" placeholder="Search here...."/>
-          <input className="bg-red-600 rounded-r-lg p-2 pl-2 pr-3 text-base font-semibold" type="submit" value=' Search'/></form>
+          <h1 className="mb-5 md:text-4xl text-center font-bold w-fit text-black">
+            I Grow By Helping People In Need
+          </h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="p-2 text-black rounded-l-lg  w-56"
+              name="keyword"
+              type="text"
+              placeholder="Search here...."
+            />
+            <input
+              className="bg-red-600 rounded-r-lg p-2 pl-2 pr-3 text-base font-semibold"
+              type="submit"
+              value=" Search"
+            />
+          </form>
         </div>
       </div>
     </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,20 +1,39 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner";
-import DonationsChart from "../DonationsChart/DonationsChart";
-
+import { useEffect, useState } from "react";
+import DonationCards from "../DonationCards/DonationCards";
 
 const Home = () => {
-    const donation = useLoaderData();
-    // console.log(donation);
-    return (
-        <div>
-            <div className=" md:max-w-7xl mx-auto">
-            <Banner></Banner>
+  const donations = useLoaderData();
 
+  const [keyword, setKeyword] = useState('');
+
+
+
+  const bringKeyword = (broughtKeyword) =>{
+    setKeyword(broughtKeyword);
+  }
+  console.log(keyword);
+
+
+  
+  
+
+  return (
+    <div>
+      <div className=" md:max-w-7xl mx-auto"><Banner getKeyword={bringKeyword} ></Banner></div>
+
+      
+      <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 mt-24 mb-24 gap-6">
+                {
+                    donations.filter((donation)=>{ 
+                        return keyword.toLowerCase() === '' ? donation : donation.category.toLowerCase().includes(keyword.toLocaleLowerCase());
+                    }).map(donation=><DonationCards key={donation.id} donation={donation}></DonationCards>)
+                }
+           
             </div>
-            <DonationsChart donation={donation} ></DonationsChart>
-        </div> 
-    );
+    </div>
+  );
 };
 
 export default Home;
